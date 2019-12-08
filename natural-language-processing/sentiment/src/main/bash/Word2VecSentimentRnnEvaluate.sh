a5292fb198a4e2d34c51b73846626660d9a714aa
Roberto Marquez <onebeartoe@gmail.com>
2019-12-07 20:24:42 -0600

    add a bash script to run the evaluation

---
+++ natural-language-processing/sentiment/src/main/bash/Word2VecSentimentRnnEvaluate.sh
@@ -0,0 +1,9 @@
+
+# the location of the sentiment word vectors is:
+#   https://deeplearning4jblob.blob.core.windows.net/resources/wordvectors/GoogleNews-vectors-negative300.bin.gz
+
+java -jar the-acutal-jar-file.jar \
+     ~/Downloads/world/deep-learning/dl4j-examples-data/dl4j-examples/GoogleNews-vectors-negative300.bin.gz \
+
+
+
