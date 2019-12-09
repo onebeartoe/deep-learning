4e297e662f76166defeb9a27ce0ac557aa435bf9
Roberto Marquez <onebeartoe@gmail.com>
2019-12-09 13:44:54 -0600

    add a placeholder for unit tests

---
+++ natural-language-processing/sentiment/src/test/java/org/onebeartoe/deep/learning/recurrent/neural/network/sentiment/SentimentServiceSpecification.java
@@ -0,0 +1,10 @@
+
+package org.onebeartoe.deep.learning.recurrent.neural.network.sentiment;
+
+/**
+ * This class verifies the SentimentService specification.
+ */
+public class SentimentServiceSpecification
+{
+    
+}
