af6a1826d982b72310eee4afe07746f4546e924a
Roberto Marquez <onebeartoe@gmail.com>
2019-12-09 13:44:16 -0600

    add resources

--- natural-language-processing/sentiment/src/main/java/org/onebeartoe/deep/learning/recurrent/neural/network/sentiment/SentimentService.java
+++ natural-language-processing/sentiment/src/main/java/org/onebeartoe/deep/learning/recurrent/neural/network/sentiment/SentimentService.java
@@ -7,5 +7,10 @@ package org.onebeartoe.deep.learning.recurrent.neural.network.sentiment;
  */
 public class SentimentService
 {
-    
+//TODO: use the WordVec
+//          /home/roberto/Downloads/world/deep-learning/dl4j-examples-data/dl4j-examples/
+
+
+//TODO: do we need to detect types of sentences?
+//          http://plato.algonquincollege.com/applications/guideToGrammar/?page_id=3243    
 }
