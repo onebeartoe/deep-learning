f1f614308d132bd5641d0ca5317191b699fa02f9
Roberto Marquez <onebeartoe@gmail.com>
2019-12-09 17:45:38 -0600

    add a name detector

---
+++ natural-language-processing/named-entity/src/main/java/org/onebeartoe/deep/learning/nlp/named/entity/PersonNameDetector.java
@@ -0,0 +1,10 @@
+
+package org.onebeartoe.deep.learning.nlp.named.entity;
+
+/**
+ *This class detects English names of people that appear in a text string.
+ */
+public class PersonNameDetector
+{
+    
+}
