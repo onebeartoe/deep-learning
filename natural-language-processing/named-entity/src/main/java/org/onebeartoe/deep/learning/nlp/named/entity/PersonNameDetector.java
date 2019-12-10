4189a176789e3919d54048014c03c83ad20a8f13
Roberto Marquez <onebeartoe@gmail.com>
2019-12-09 21:09:32 -0600

    add a name detector

--- natural-language-processing/named-entity/src/main/java/org/onebeartoe/deep/learning/nlp/named/entity/PersonNameDetector.java
+++ natural-language-processing/named-entity/src/main/java/org/onebeartoe/deep/learning/nlp/named/entity/PersonNameDetector.java
@@ -1,6 +1,22 @@
 
 package org.onebeartoe.deep.learning.nlp.named.entity;
 
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
+
+import opennlp.tools.namefind.NameFinderME; 
+import opennlp.tools.namefind.TokenNameFinderModel; 
+import opennlp.tools.tokenize.TokenizerME; 
+import opennlp.tools.tokenize.TokenizerModel; 
+import opennlp.tools.util.Span;  
+
+import opennlp.tools.namefind.NameFinderME; 
+import opennlp.tools.namefind.TokenNameFinderModel; 
+import opennlp.tools.util.Span;  
+
 /**
  *This class detects English names of people that appear in a text string.
  */
---
+++ natural-language-processing/named-entity/src/test/java/org/onebeartoe/deep/learning/nlp/named/entity/PersonNameDetectorSpecification.java
@@ -0,0 +1,18 @@
+
+package org.onebeartoe.deep.learning.nlp.named.entity;
+
+import org.testng.annotations.Test;
+
+/**
+ * This class tests the PersonNameDetector specification.
+ */
+public class PersonNameDetectorSpecification
+{
+    private PersonNameDetector implementation;
+    
+    @Test
+    public void testSomeMethod()
+    {
+        
+    }    
+}
