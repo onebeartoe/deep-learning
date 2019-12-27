9315117bb50e0cdce56871ec920d825379ec9d83
Roberto Marquez <onebeartoe@gmail.com>
2019-12-26 22:18:45 -0600

    cleanup

--- natural-language-processing/language-detection/src/main/java/org/onebeartoe/deep/learning/nlp/language/detection/LanguageDetectionService.java
+++ natural-language-processing/language-detection/src/main/java/org/onebeartoe/deep/learning/nlp/language/detection/LanguageDetectionService.java
@@ -5,16 +5,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import opennlp.tools.langdetect.Language;
 
 import opennlp.tools.langdetect.LanguageDetector;
