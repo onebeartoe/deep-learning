e4534e4042d89fca6400b5153c9f74d644aa3841
onebeartoe <onebeartoe@gmail.com>
2023-02-13 15:22:34 +0000

    cleanup

---
+++ gan/djl/src/test/java/BigGanSpecification.java
@@ -0,0 +1,11 @@
+
+import ai.djl.examples.inference.BigGAN;
+import ai.djl.modality.cv.Image;
+
+
+public class BigGanSpecification 
+{
+
+    Image[] generatedImages = BigGAN.generate();    
+    
+}
