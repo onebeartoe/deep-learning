23fa318f8cea190d7e516eb8aa91b106df8a10d3
onebeartoe <onebeartoe@gmail.com>
2023-02-13 16:21:39 +0000

    correct the test configuration

--- gan/djl/src/test/java/ai/djl/examples/inference/BigGanSpecification.java
+++ gan/djl/src/test/java/ai/djl/examples/inference/BigGanSpecification.java
@@ -13,6 +13,19 @@ import org.testng.annotations.Test;
 public class BigGanSpecification 
 {
     @Test
+    /**
+     * 
+     * This test ensures the model files are available.
+     * 
+     * If this test fails make sure the following dependecies are in the POM file:
+     * 
+     *      ai.djl:basicdataset:0.19.0
+     * 
+     *      ai.djl.pytorch:pytorch-model-zoo:0.19.0
+     * 
+     *      ai.djl:model-zoo:0.19.0         
+     * 
+     */
     public void generate_success() throws IOException, ModelException, TranslateException
     {
         Image[] generatedImages = BigGAN.generate();    
