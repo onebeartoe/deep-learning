d76a89bd96aa2e7f024a5386acabe6ed8d8fc8b7
onebeartoe <onebeartoe@gmail.com>
2023-02-16 17:54:01 +0000

    whitespace

--- gan/djl-desktop/src/main/java/org/onebeartoe/deep/learning/gan/djl/desktop/App.java
+++ gan/djl-desktop/src/main/java/org/onebeartoe/deep/learning/gan/djl/desktop/App.java
@@ -24,17 +24,20 @@ public class App extends Application
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    static void setRoot(String fxml) throws IOException 
+    {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static Parent loadFXML(String fxml) throws IOException 
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
-
 }
