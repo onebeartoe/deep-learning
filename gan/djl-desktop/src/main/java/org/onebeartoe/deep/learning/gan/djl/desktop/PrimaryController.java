7ecbbfd26cc17bc22ed503a7f1f6515a8d27eec9
onebeartoe <onebeartoe@gmail.com>
2023-02-16 05:38:31 +0000

    add an in-memory search feature

--- gan/djl-desktop/src/main/java/org/onebeartoe/deep/learning/gan/djl/desktop/PrimaryController.java
+++ gan/djl-desktop/src/main/java/org/onebeartoe/deep/learning/gan/djl/desktop/PrimaryController.java
@@ -1,10 +1,60 @@
 package org.onebeartoe.deep.learning.gan.djl.desktop;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PrimaryController 
+        implements Initializable 
 {
+
+//TODO: document the in-memory search feature.
+//          https://gist.github.com/Da9el00/cde4381b8f17299693bab886ec700a78    
+    
+    ArrayList<String> words = new ArrayList<>(
+            Arrays.asList("test", "dog","Human", "Days of our life", "The best day",
+                    "Friends", "Animal", "Human", "Humans", "Bear", "Life",
+                    "This is some text", "Words", "222", "Bird", "Dog", "A few words",
+                    "Subscribe!", "SoftwareEngineeringStudent", "You got this!!",
+                    "Super Human", "Super", "Like")
+    );
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private ListView<String> listView;
+
+    @FXML
+    void search(ActionEvent event) {
+        listView.getItems().clear();
+        listView.getItems().addAll(searchList(searchBar.getText(),words));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listView.getItems().addAll(words);
+    }
+
+    private List<String> searchList(String searchWords, List<String> listOfStrings) {
+
+        List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
+
+        return listOfStrings.stream().filter(input -> {
+            return searchWordsArray.stream().allMatch(word ->
+                    input.toLowerCase().contains(word.toLowerCase()));
+        }).collect(Collectors.toList());
+    }
+
 
     @FXML
     private void switchToSecondary() throws IOException 
