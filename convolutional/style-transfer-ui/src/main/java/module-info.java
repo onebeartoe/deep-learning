c96fce7134d2cddaec1740b907ec94d7198203b5
onebeartoe <onebeartoe@gmail.com>
2023-03-04 16:32:36 +0000

    add some exlusions

--- convolutional/style-transfer-ui/src/main/java/module-info.java
+++ convolutional/style-transfer-ui/src/main/java/module-info.java
@@ -1,33 +1,19 @@
 module org.onebeartoe.deep.learning.convolutional.style.transfer.desktop
-{
-//    requires ai.djl.api;
-    
-//TODO: update the name of this 'requires' to org.onebeartoe.deep.learning.gan.djl
-//    requires gandjl;
-
+{    
     requires java.desktop;
     
     requires java.logging;
    
-//    requires java.*;
-    
     requires javafx.controls;
     requires javafx.fxml;
-//    requires javafx.swing;
-
     
     requires datavec.data.image;
     
     requires deeplearning4j.common;
-//    requires deeplearning4j.nn;
-//    requires deeplearning4j.util;
-    
+
     // you can find the 'requires' for a non-Java-module JAR by issusing this command:
     //      $ jar --file=slf4j-api-1.7.36.jar --describe-module
     // Then you copy and paste whatever is before the '@' symbol.    
-    
-//    opens org.onebeartoe.deep.learning.style.transfer.ui to nd4j.api;
-  //  opens org.onebeartoe.deep.learning.style.transfer.ui to deeplearning4j.nn;
     
     opens org.onebeartoe.deep.learning.style.transfer.ui to javafx.fxml;
     
---
+++ convolutional/style-transfer-ui/src/test/java/org/deeplearning4j/examples/styletransfer/NeuralStyleTrnasferSpecification.java
@@ -0,0 +1,16 @@
+
+package org.deeplearning4j.examples.styletransfer;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ */
+public class NeuralStyleTrnasferSpecification 
+{
+    @Test
+    public void vigorousTest()
+    {
+        
+    }    
+}
